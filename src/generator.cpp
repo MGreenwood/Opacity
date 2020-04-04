3dacc873cd0ce97b7daa9fd0a81ac0c34370d6f3
mgreenwood <mgreenwood.dev@gmail.com>
2020-04-04 13:55:35 -0400

    removed a couple symbols that arent always supported on keybaords

--- src/generator.cpp
+++ src/generator.cpp
@@ -13,7 +13,7 @@ vector<char> upper {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                       'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                       'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 vector<char> symbols   { '!', '@', '%', '^', '&', '*', '[', ']', '{', '}',
-                       '(', ')', '_', '+', '-', '=', '<', '>', '?' };
+                       '(', ')', '_', '+', '-', '=', '?' };
 vector<char> numbers {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 unsigned int minOfEach = 2;
 unsigned int minLength = 8;
